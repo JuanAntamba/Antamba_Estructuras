8c538c77df4117dfca7fe0200847edadb1ba6eef
Juan Carlos Antamba <juan.antamba@udla.edu.ec>
2024-01-16 15:28:30 -0500

    Se crea el menu en pantalla seguido de switch para todos los casos de opciones, y se crea el primer case, donde se guarda la informacion de los alumnos en el arreglo creado

--- estructuras.c
+++ estructuras.c
@@ -9,4 +9,40 @@ int main() {
         char carrera[50];
         float promedio;
     } alumnos[100];  // Crear un arreglo de alumnos
+
+    int n = 0;  
+    int opcion;  
+
+    do {
+        // Mostramos el menu que elegira el usuario , este tendra dos opciones
+        puts("\n1. Ingresar alumno");
+        puts("2. Ver alumnos ingresados");
+        puts("3. Salir");
+        puts("Seleccione una opcion: ");
+        scanf("%d", &opcion);
+        getchar(); 
+
+        switch (opcion) {
+            case 1:  // Ingresar alumno y se guarde los datos, utilizamos el arreglo.
+                if (n < 100) {
+                    printf("\nIngrese los datos del alumno %d:\n", n + 1);
+                    puts("Ingrese la matricula: ");
+                    scanf("%d", &alumnos[n].matricula);
+                    getchar();  
+                    puts("Ingrese el nombre: ");
+                    gets(alumnos[n].nombre);
+                    puts("Ingrese la direccion: ");
+                    gets(alumnos[n].direccion);
+                    puts("Ingrese la carrera: ");
+                    gets(alumnos[n].carrera);
+                    puts("Ingrese el promedio: ");
+                    scanf("%f", &alumnos[n].promedio);
+                    getchar(); 
+                    n++;
+                } else {
+                    puts("No se pueden ingresar más alumnos.");
+                }
+                break;
+        }
+    }
 }
