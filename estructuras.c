652ab867f1d04c63812e1fb39510ee55d05b2488
Juan Carlos Antamba <juan.antamba@udla.edu.ec>
2024-01-16 15:25:22 -0500

    En esta aparte se creea la estrucutra de todos los datos del usuario, ademas del arreglo que contendra todos los alumnos

---
+++ estructuras.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    struct alumno {
+        int matricula;
+        char nombre[50];
+        char direccion[50];
+        char carrera[50];
+        float promedio;
+    } alumnos[100];  // Crear un arreglo de alumnos
+}
