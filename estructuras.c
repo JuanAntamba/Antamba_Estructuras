4405264d6def5a4c157003857ff7aa2e0c1f0b92
Juan Carlos Antamba <juan.antamba@udla.edu.ec>
2024-01-16 20:47:11 -0500

    Se modifico el codigo, se reemplazo gets por fgets, debido a la advertencia de seguridad de gets.Ademas, se utilizo strcspn para eliminar el caracter de nueva linea'\n'que fgets incluye al final de la cadena

--- estructuras.c
+++ estructuras.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    
     struct alumno {
         int matricula;
         char nombre[50];
@@ -10,17 +10,17 @@ int main() {
         float promedio;
     } alumnos[100];  // Crear un arreglo de alumnos
 
-    int n = 0;  
-    int opcion;  
+    int n = 0; 
+    int opcion; 
 
     do {
-        // Mostramos el menu que elegira el usuario , este tendra dos opciones
+         // Mostramos el menu que elegira el usuario , este tendra dos opciones
         puts("\n1. Ingresar alumno");
         puts("2. Ver alumnos ingresados");
         puts("3. Salir");
         puts("Seleccione una opcion: ");
         scanf("%d", &opcion);
-        getchar(); 
+        getchar();  // Para consumir el salto de linea que se queda en el buffer después de scanf
 
         switch (opcion) {
             case 1:  // Ingresar alumno y se guarde los datos, utilizamos el arreglo.
@@ -28,22 +28,25 @@ int main() {
                     printf("\nIngrese los datos del alumno %d:\n", n + 1);
                     puts("Ingrese la matricula: ");
                     scanf("%d", &alumnos[n].matricula);
-                    getchar();  
+                    getchar(); 
                     puts("Ingrese el nombre: ");
-                    gets(alumnos[n].nombre);
+                    fgets(alumnos[n].nombre, 50, stdin);
+                    alumnos[n].nombre[strcspn(alumnos[n].nombre, "\n")] = 0; // Eliminar el '\n' al final
                     puts("Ingrese la direccion: ");
-                    gets(alumnos[n].direccion);
+                    fgets(alumnos[n].direccion, 50, stdin);
+                    alumnos[n].direccion[strcspn(alumnos[n].direccion, "\n")] = 0; 
                     puts("Ingrese la carrera: ");
-                    gets(alumnos[n].carrera);
+                    fgets(alumnos[n].carrera, 50, stdin);
+                    alumnos[n].carrera[strcspn(alumnos[n].carrera, "\n")] = 0;  
                     puts("Ingrese el promedio: ");
                     scanf("%f", &alumnos[n].promedio);
-                    getchar(); 
+                    getchar();  
                     n++;
                 } else {
                     puts("No se pueden ingresar más alumnos.");
                 }
                 break;
-            case 2:  // for para poder ver alumnos ingresados
+            case 2:  // Ver alumnos ingresados
                 puts("\nDatos de los alumnos ingresados:\n");
                 for (int i = 0; i < n; i++) {
                     printf("\nAlumno %d:\n", i + 1);
@@ -54,7 +57,7 @@ int main() {
                     printf("Promedio: %.2f\n", alumnos[i].promedio);
                 }
                 break;
-            case 3:  
+            case 3:  // Salir
                 puts("Saliendo del programa.");
                 break;
             default:
